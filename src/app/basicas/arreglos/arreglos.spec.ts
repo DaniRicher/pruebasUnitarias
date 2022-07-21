14e5be0f2b678d071a175b655a517904d6fa5904
DaniRicher <richerdani3@gmail.com>
2022-07-20 21:52:33 -0500

    Pruebas unitarias

---
+++ src/app/basicas/arreglos/arreglos.spec.ts
@@ -0,0 +1,21 @@
+import { obtenerRobots } from "./arreglos";
+
+
+describe('Pruebas de arreglos', () => {
+
+    it('Debe retonar al menos 3 robots', () => {
+
+        const resp = obtenerRobots();
+        expect ( resp.length ).toBeGreaterThanOrEqual( 3 );
+
+    });
+
+    it('Debe de existir MegaMan y Ultron', () => {
+
+        const resp = obtenerRobots();
+        expect ( resp ).toContain('MegaMan');
+        expect ( resp ).toContain('Ultron');
+
+    });
+
+});
